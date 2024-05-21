a4f9ec5957045877e75a2f547fc6bef11df71bb7
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-05-22 00:32:55 +0200

    dw

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register() {
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => 'required|max:20',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:50'
+        ]);
         return 'thanks you new user';
     }
 }
