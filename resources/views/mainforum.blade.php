b01d5076fde4df988762571c23f0826673dc2ecb
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2024-05-22 23:50:00 +0200

    database login, forum and much more

--- resources/views/mainforum.blade.php
+++ resources/views/mainforum.blade.php
@@ -3,12 +3,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <link rel="stylesheet" href="mainforum.css">
+    <title>Forum | All Posts</title>
 </head>
 <body>
-
     @auth
-    <header id="headercontainer">
+    <header>
         <p>You're logged in</p>
         <form action="/logout" method="POST">
             @csrf
@@ -17,27 +17,11 @@
     </header>
 
     <div style="border: 3px solid black;">
-        <h2>Create a new post</h2>
-        <form action="/create-post" method="POST">
-            @csrf
-            <input name="title" type="text" placeholder="title">
-            <input name="body" type="text" placeholder="body content">
-            <button>Create</button>
-        </form> 
-    </div>
-
-    <div style="border: 3px solid black;">
         <h2>All Posts</h2>
         @foreach ($posts as $post)
         <div style="background-color: gray; padding: 10px; margin: 10px">
-            <h3 style="text-decoration: underline">{{ $post->title }} by {{$post->user->name}}</h3>
+            <h3 style="text-decoration: underline">{{ $post->title }}</h3>
             <p>{{ $post->body }}</p>
-            <p><a href="/edit-post/{{$post->id}}">Edit Post</a></p>
-            <form action="/delete-post/{{$post->id}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button>Delete Post</button>
-            </form>
         </div>
     </div>
     @endforeach
@@ -63,10 +47,6 @@
             <button>Login</button>
         </form>
     </div>
-
     @endauth
-
-    
-    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,37 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('home');
+    $åpsts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->userPosts()->latest()->get();
+    } else {
+        $posts = Post::latest()->get();
+    }
+    // actually works even with userPosts as an error (reason unknown)
+    //$posts = auth()->user()->userPosts()->latest()->get();
+    //$posts = Post::where('user_id', auth()->id())->get();
+    return view('home', ['posts' => $posts]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
+
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::post('/login', [UserController::class, 'login']);
+
+Route::get('/mainforum', function () {
+    $posts = Post::all();
+    return view('home', ['posts' => $posts]);
+});
+
+
+// Forum posts routes
+Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'updatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
